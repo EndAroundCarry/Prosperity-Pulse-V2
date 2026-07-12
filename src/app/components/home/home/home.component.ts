ce336e5f203def2f53c28e1a01293f012e616abd
Bogdan Gantu <gantubogdan@gmail.com>
2026-07-12 14:44:01 +0300

    change homepage design

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NewsFeedComponent } from '../../news-feed/news-feed.component';
 import { RouterModule } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   imports: [
-    RouterModule
+    RouterModule,
+    MatIcon
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
